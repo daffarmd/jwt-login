d2ff66ebfbeb7fb62508f6e0acb784a87e2f10c0
daffarmd <muhammaddaffarmd@gmail.com>
2025-07-05 09:28:28 +0700

    test(user): modified test user to repository test

--- internal/repository/user_repository_test.go
+++ internal/repository/user_repository_test.go
@@ -1,33 +1,15 @@
 package repository
 
 import (
-	"database/sql"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/daffarmd/task-manager/internal/model"
 	_ "github.com/lib/pq"
 )
 
-var testRepo UserRepository
-
-func TestMain(m *testing.M) {
-	// Connect ke test DB
-	db, err := sql.Open("postgres", "host=localhost port=5432 user=devaja password=1q2w3e4r dbname=taskmanager sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	testRepo = NewUserRepository(db)
-
-	code := m.Run()
-	os.Exit(code)
-}
-
 func TestUserRepository_CreateAndFindByEmail(t *testing.T) {
 	// Arrange
-	email := "unittest@example.com"
+	email := "unittest1@example.com"
 	user := model.User{
 		Name:     "Test User",
 		Email:    email,
@@ -35,13 +17,13 @@ func TestUserRepository_CreateAndFindByEmail(t *testing.T) {
 	}
 
 	// Act
-	err := testRepo.Create(user)
+	err := userRepoTest.Create(user)
 	if err != nil {
 		t.Fatalf("error creating user: %v", err)
 	}
 
 	// Assert
-	got, err := testRepo.FindByEmail(email)
+	got, err := userRepoTest.FindByEmail(email)
 	if err != nil {
 		t.Fatalf("error finding user: %v", err)
 	}
